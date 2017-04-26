b948904f71cc7d4206e1b30d1aed8968d7802b7a
Brian Rush and Justin Gomez <justin@>
2017-04-26 14:58:00 -0700

    add functionality for subtraction, multiplication, and division

--- js/scripts.js
+++ js/scripts.js
@@ -5,45 +5,51 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-// alert(add(number1, number2));
-//
-//
-// var subtract = function(number1, number2) {
-//   return number1 - number2;
-// };
-//
-// var number1 = parseFloat(prompt("Enter a number:"));
-// var number2 = parseFloat(prompt("Enter another number:"));
-//
-// alert(subtract(number1, number2));
-//
-// var multiply = function(number1, number2) {
-//   return number1 * number2;
-// };
-//
-// var number1 = parseFloat(prompt("Enter a number:"));
-// var number2 = parseFloat(prompt("Enter another number:"));
-//
-// alert(multiply(number1, number2));
-//
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// };
-//
-// var number1 = parseFloat(prompt("Enter a number:"));
-// var number2 = parseFloat(prompt("Enter another number:"));
-//
-// alert(divide(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
 // UI Logic
+// Addition
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseFloat($("#add1").val());
+    var number2 = parseFloat($("#add2").val());
     var result = add(number1, number2);
+    $("#output").text(result);
+  });
+  // Subtract
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseFloat($("#subtract1").val());
+    var number2 = parseFloat($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  // Multiply
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseFloat($("#multiply1").val());
+    var number2 = parseFloat($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  // Divide
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseFloat($("#divide1").val());
+    var number2 = parseFloat($("#divide2").val());
+    var result = divide(number1, number2);
     $("#output").text(result);
   });
 });
