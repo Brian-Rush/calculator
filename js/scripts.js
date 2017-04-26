e2e05eecbebcb29f5fb47ad5fea00731c3047271
Brian Rush and Justin Gomez <brian@>
2017-04-26 14:39:10 -0700

    add complete functionality for addition and some html for other arithmetic

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var number1 = parseFloat(prompt("Enter a number:"));
-// var number2 = parseFloat(prompt("Enter another number:"));
-//
-// alert(add(number1, number2));
 
+// Business Logic
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+
+// alert(add(number1, number2));
+//
+//
 // var subtract = function(number1, number2) {
 //   return number1 - number2;
 // };
@@ -24,12 +26,24 @@
 // var number2 = parseFloat(prompt("Enter another number:"));
 //
 // alert(multiply(number1, number2));
+//
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
+//
+// var number1 = parseFloat(prompt("Enter a number:"));
+// var number2 = parseFloat(prompt("Enter another number:"));
+//
+// alert(divide(number1, number2));
 
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
 
-var number1 = parseFloat(prompt("Enter a number:"));
-var number2 = parseFloat(prompt("Enter another number:"));
-
-alert(divide(number1, number2));
+// UI Logic
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
